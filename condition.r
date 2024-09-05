155174935356dfa2f4816cd4b5a00da8c6d77d2a
shashankssp12 <122225481+shashankssp12@users.noreply.github.com>
2024-09-05 15:12:50 +0530

    Refactor conditional operator usage for three numbers

--- condition.r
+++ condition.r
@@ -1,8 +1,11 @@
 # Conditional Operators Usage
 a <- 10
 b <- 20
-if (a>b) {
-   print("a is greater than b")
-} else {    
-   print("b is greater than a")
+c <- 30
+if (a>b && a>c) {
+   print("a is greater than b and c")
+} else if (b > a && b > c) {    
+   print("b is greater than a and c")
+} else {
+   print("c is greater than a and b")
 }
