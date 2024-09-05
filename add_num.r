5ccdec47698dd9566195d44215f42253e1d5ecb4
shashankssp12 <122225481+shashankssp12@users.noreply.github.com>
2024-09-05 15:01:32 +0530

    feat: Add code for adding two numbers and conditional operator usage

---
+++ add_num.r
@@ -0,0 +1,44 @@
+# # Define the two numbers
+# num1 <- 5
+# num2 <- 10
+
+# # Add the numbers
+# result <- num1 + num2
+
+# # Print the result
+# print(result)
+
+
+# x <- 5
+# y <- 6
+# a <- 3
+# b <- 4 
+# z <- x+y
+# print(paste("Sum=",x+y, "Value=",z))
+
+# cat("a=",a, "\nb=",b, "\nz=",z)
+
+
+# d <- 16%%3
+# m <- 2^3
+# cat("\n", m)
+# cat("\n", d)
+
+# Comparison Operator
+# >,<, >=,<=, !=, ==
+
+# LOGICAL OPERATOR 
+# &,|, !
+
+# cat((x>y) & (y>2))
+# cat((x>y) | (y>2))
+# cat("\n")
+# cat(!(x>y))
+# cat("\n")
+
+
+# Boolean Operator
+bool1 <- TRUE
+bool2 <- FALSE
+print(bool1 & bool2)
+print(bool1 | bool2)
---
+++ condition.r
@@ -0,0 +1,8 @@
+# Conditional Operators Usage
+a <- 10
+b <- 20
+if (a>b) {
+   print("a is greater than b")
+} else {    
+   print("b is greater than a")
+}
